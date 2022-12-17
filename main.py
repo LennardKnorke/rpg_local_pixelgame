45d845ad601f543359e947799bbbf20d43774036
LennardKnorke <janlennardgrabowski@gmx.de>
2022-12-17 01:04:52 +0100

    comments added, small adjustment to var names

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from config_func import *
 from server import *
-from game_classes import *
+from local_game import *
 from menu import *
 #Game Loop
     #Update window
@@ -11,54 +11,49 @@ from menu import *
     #Receive from server
 
 #   -   Start
-#Load settings and configs
-#find account or create new one
+#Load window and pygame features
     #   -   Menu
     #Play
-        #Create
-            #decide local ID and port
-            #Run server
-            #Join as Client
-            #Start Game Loop
-        #Join
-            #Enter ip and port
-            #Start Client
-            #Start Game Loop
-    #Exit
-#Some Base Values, which wont change!
+        #Decide between adventure and vs mode
+            #decide to join or host
+                    #Run server
+                #Join as Client
+                #Start Game Loop
+                #at the end of the game loop exit all servers and based on command return to menu or exit
+
+
+
+#Getting/setting some base specs used to process the window
 pygame.init()
 SCREEN_SIZE = get_monitor_specs()
 SCREEN_HEIGHT = SCREEN_SIZE[1]
 SCREEN_WIDTH = SCREEN_SIZE[0]
+WINDOW = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 FPS = 30
 CLOCK = pygame.time.Clock()
-MUSICBOX = pygame.mixer.init()
-
+#Used to play
+pygame.mixer.init()
+#Used to render text
 pygame.font.init()
 GAME_FONT = pygame.font.Font('sprites/IMMORTAL.ttf', int(32*(SCREEN_SIZE[0] / 1920)))
-WINDOW = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 pygame.mouse.set_visible(False)
 RUNNING = True
 while RUNNING:
-    next = Menu(WINDOW, SCREEN_SIZE, CLOCK, GAME_FONT)
+    main_arguments = Menu(WINDOW, SCREEN_SIZE, CLOCK, GAME_FONT)
     #Based on Menu input changed between ending the game, running game as a client or server host
     #IF 0 End Application
-    if next[0] == 0:
+    if main_arguments[0] == 0:
         RUNNING = False
     #If 1, start adventure as host
-    elif next[0] == 1:
-        ADVENTURE(hosting = True, ip = next[1], port = [2], SCREEN = WINDOW, SCREEN_SIZE = SCREEN_SIZE, clock = CLOCK, font_render = GAME_FONT)
+    elif main_arguments[0] == 1:
+        adventure(hosting = True, ip = main_arguments[1], port = [2], SCREEN = WINDOW, SCREEN_SIZE = SCREEN_SIZE, clock = CLOCK, font_render = GAME_FONT, Player_number = 0)
     #If 2, join adventure as client
-    elif next[0] == 2:
-        ADVENTURE(hosting = False, ip = next[1], port = [2], SCREEN = WINDOW, SCREEN_SIZE = SCREEN_SIZE, clock = CLOCK, font_render = GAME_FONT)
+    elif main_arguments[0] == 2:
+        adventure(hosting = False, ip = main_arguments[1], port = [2], SCREEN = WINDOW, SCREEN_SIZE = SCREEN_SIZE, clock = CLOCK, font_render = GAME_FONT)
 
-    #If 3, run Versus maps? (Big construction)
-    elif next[0] == 3:
+    #If 3, run Versus maps? (Big construction). Do later or do we need it?
+    elif main_arguments[0] == 3:
         RUNNING = False
-
-
-
-        
-    pygame.display.update()
 
 pygame.quit()
