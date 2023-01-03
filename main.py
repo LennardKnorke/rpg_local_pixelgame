25f7be741ef855f54b475f1eaa19170e56bedaea
LennardKnorke <106014763+LennardKnorke@users.noreply.github.com>
2023-01-03 18:49:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import server
         #Host or Join
         #If Host, exit menu and start the shit show
         #If join, enter ip and port
-    #3. Server Settings
+    #3. Game Cycle
         #If Hosting, start server process. SEPERATE PROCESS STARTS HERE!!:
             #runs server_main function from server.py
             #create a server class instance
@@ -41,32 +41,23 @@ import server
 if __name__ == '__main__':
     #get ip-4 of mashine and prepare 2 sockets to reserver a portnumber for client and server sockets
     #Use one as a client socket to connect to a host
-    HOST_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #HOST_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     CLIENT_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    HOST_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     LOKAL_IP = find_local_host()
-    HOST_IP = LOKAL_IP
-    searching_ports = [True, True]
     print("Local Ip: ", LOKAL_IP)
-    while searching_ports[0] == True or searching_ports[1] == True:
-        if searching_ports[0] == True:#search port for client
-            try:
-                CLIENT_SOCKET.bind((LOKAL_IP, 0))
-                CLIENT_PORT = CLIENT_SOCKET.getsockname()[1]
-                print("Client_port: ", CLIENT_PORT)
-                searching_ports[0] = False
-            except:
-                pass
-        if searching_ports[1] == True:#search port for client
-            try:
-                HOST_SOCKET.bind((LOKAL_IP, 0))
-                HOST_PORT = HOST_SOCKET.getsockname()[1]
-                print("As needed server port: ", HOST_PORT)
-                searching_ports[1] = False
-            except:
-                pass
-    server_process = Process(target = server.server_main, args = (HOST_IP, HOST_PORT, HOST_SOCKET))
+
+    CLIENT_SOCKET.bind((LOKAL_IP, 0))
+    CLIENT_PORT = CLIENT_SOCKET.getsockname()[1]
+
+    HOST_SOCKET.bind((LOKAL_IP, 0))
+    HOST_PORT = HOST_SOCKET.getsockname()[1]
+    print("Client_port: ", CLIENT_PORT)
+    print("Host_port: ", HOST_PORT)
+
+    server_process = Process(target = server.server_main, args = (HOST_SOCKET,True))
     #keep a copy of reserved host socket port!
-    host_ip_cpy = HOST_IP
+    host_ip_cpy = LOKAL_IP
     host_port_cpy = HOST_PORT
     #Setting windows specks
     pygame.init()
@@ -83,7 +74,7 @@ if __name__ == '__main__':
     GAME_FONT = pygame.font.Font('sprites/IMMORTAL.ttf', int(32 * creator_to_user_ratio_width))
     pygame.mouse.set_visible(False)
 
-
+    #Main application loop. swithces between menu and game application
     APP_RUNNING = True
     while APP_RUNNING:
         main_arguments = Menu(WINDOW, SCREEN_SIZE, SCREEN_RATIOS, CLOCK, GAME_FONT)
@@ -96,11 +87,12 @@ if __name__ == '__main__':
                 server_process.start()
             #otherwise save the host ip and port number
             else:
-                HOST_IP = main_arguments[2]
+                #How to handle ip adress?
+                #HOST_IP = main_arguments[2]
                 HOST_PORT = main_arguments[3]
 
-            Game = adventure(window = WINDOW, window_size = SCREEN_SIZE, window_ratios = SCREEN_RATIOS, clock = CLOCK, font_render = GAME_FONT, c_socket = CLIENT_SOCKET,c_ip = LOKAL_IP, c_port = CLIENT_PORT, host_ip = HOST_IP, host_port = HOST_PORT)
-            Game.connect_client()
+            Game = adventure(window = WINDOW, window_size = SCREEN_SIZE, window_ratios = SCREEN_RATIOS, clock = CLOCK, font_render = GAME_FONT, c_socket = CLIENT_SOCKET,c_ip = LOKAL_IP, c_port = CLIENT_PORT)
+            Game.connect_client(HOST_PORT)
             if Game.connected:
                 Game.run()
             #After the game aint running anymore, check if the desire was to quit?
